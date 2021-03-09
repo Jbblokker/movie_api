434997f09533c20df06eecfe7c6cfca3d819eac9
Jbblokker <jbblokker@outlook.com>
2021-03-09 05:28:45 -0600

    Added requests
    added multiple request to api to make it more complete

--- index.js
+++ index.js
@@ -1,5 +1,6 @@
 const express = require('express');
 morgan = require('morgan');
+uuid = require('uuid');
 
 const app = express();
 
@@ -48,21 +49,84 @@ const topMovies = [
   }
 ];
 
-//get Requests
+//request list of all movies
 app.get('/movies', (req, res) => {
   res.json(topMovies);
 });
-
+//request sending back message
 app.get('/', (req, res) => {
   res.send('You have chosen an excellent selection of movies!')
 });
 
+//request displaying the documentation
 app.use(express.static('public/documentation.html'));
 
+//request data about a genre
+app.get('movies/:title', (req, res) => {
+  res.send('Successfully received data on a genre. ');
+});
+
+//request displaying infromation about the director
+app.get('movies/:director', (req, res) => {
+  res.send('SUccessfully retreived information about a specific director.')
+});
+
+//request data about a specific movie by title
+app.get('/movies/:name', (req, res) => {
+  res.json(movie.find((movie) =>
+    { return student.name === req.params.name}));
+});
+
+//allow useer to remove a movie from their favorites list
+app.delete('/movies/:favorites', (req, res) => {
+  res.send('Successfully deleted request on removing movie from favorites list.');
+});
+
+//allow a user to update their username.
+app.put('/users/:username', (req,res) => {
+  let users = users.find((user)=> {return user.username
+  === req.parmas,name});
+
+if (users) {
+  users.name[req.params.username] = parseInt(req.params.name);
+  res.status(201).send('users name has been updated form ' + req.params.name+ 'with new name of '
++ req.params.name);
+} else {
+  res.status(404).send('username ' + req.params.username + 'was not found')
+}
+});
+
+//allows a new user to register
+app.post('/users', (req, res) =>{
+  let newUsers = req.body;
+
+  if(!newUser.username) {
+    const message = 'missing name in request body';
+    res.status(400).send (message);
+  }else{
+    newUser.username = uuid.v4();
+    users.push(newUser);
+    res.status(201).send(newUser);
+  }
+});
+
+//allow a user to deregister.
+app.delete('/users/:username', (req, res) => {
+  res.send('User has successfully been deleted.')
+});
+
+//allow user to add movie to their favorites list
+app.post('/movies/:favorites',(req, res) => {
+  res.send('You have successfully added a movie to your favorites list.')
+});
+
+
+//error message
 app.use((err, req, res, next) => {
-  consol.error(err.stack);
+  console.error(err.stack);
   res.status(500).send('Well, that esclated quickly.')
 });
+
 
 //Listen for requests
 app.listen(8080, () => {
