800cebbabe683744bbfe7b89bbac37808c91c36d
Jbblokker <jbblokker@outlook.com>
2021-03-31 22:49:52 -0500

    changing ports that app listens on

--- index.js
+++ index.js
@@ -237,6 +237,7 @@ app.use((err, req, res, next) => {
 
 
 //Listen for requests
-app.listen(8080, () => {
-  console.log('This app is is on port 8080.');
+const port = process.env.PORT || 8080;
+app.listen(port, '0.0.0.0',() => {
+  console.log('Listening on Port ' + port);
 });
