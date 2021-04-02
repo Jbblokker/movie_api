72bee5e75f97a2bef30dec55e4800a496eb304f5
Jbblokker <jbblokker@outlook.com>
2021-04-02 06:05:09 -0500

    remove duplicated code.

--- models.js
+++ models.js
@@ -1,14 +1,6 @@
 const mongoose = require('mongoose');
 const bcrypt = require ('bcrypt');
 
-let userSchema = mongoose.Schema({
-  Username: {type: String, required: true},
-  Password: {type: String, required: true},
-  Email: {type: String, required: true},
-  Birthday: Date,
-  FavoreiteMovies:[{ type: mongoose.Schema.Types. ObjectId, ref: 'Movie'}]
-});
-
 userSchema.statics.hashPassword = (password) => {
   return bcrypt.hashSync(password, 10);
 };
