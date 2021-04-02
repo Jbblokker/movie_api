0a227514247bd73658d91def81d576f793445180
Jbblokker <jbblokker@outlook.com>
2021-04-02 05:53:48 -0500

    made notes more clear

--- index.js
+++ index.js
@@ -237,7 +237,7 @@ app.use((err, req, res, next) => {
 });
 
 
-//Listen for requests
+//Listen for requests on specific ports
 const port = process.env.PORT || 8080;
 app.listen(port, '0.0.0.0',() => {
   console.log('Listening on Port ' + port);
