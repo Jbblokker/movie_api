6b7dbe0f6ac7672101e7961949f7e82ebbe0c832
Jbblokker <jbblokker@outlook.com>
2021-04-11 13:18:28 -0500

    update in users

--- index.js
+++ index.js
@@ -156,7 +156,7 @@ app.post('/user',
       return res.status(422).json({ errors: errors.array() });
     }
 
-  let hashedPassword = hashPassword(req.body.Password);
+  let hashedPassword = Users.hashPassword(req.body.Password);
   Users.findOne({ Username: req.body.Username })//search to see if a user with the requested username already exists
     .then((user) => {
       if (user) {
