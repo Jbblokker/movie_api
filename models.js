35c16e88285204acf7202194cbf2235d95979f3d
Jbblokker <jbblokker@outlook.com>
2021-04-12 06:48:46 -0500

    updated spelling errors

--- models.js
+++ models.js
@@ -21,7 +21,7 @@ let userSchema = mongoose.Schema({
   Password: {type: String, required: true},
   Email: {type: String, required: true},
   Birthday: Date,
-  FavoreiteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
+  FavoriteMovies: [{type: mongoose.Schema.Types.ObjectId, ref: 'Movie'}]
 });
 
 userSchema.statics.hashPassword = (password) => {
