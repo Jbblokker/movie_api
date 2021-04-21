c76bfe120ff41ca638d620f8debdcbd93b049cba
Jbblokker <jbblokker@outlook.com>
2021-04-21 06:30:51 -0500

    removed jwt authentication

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ app.use(cors({
   }
 }));
 //request list of all movies
-app.get('/movies', passport.authenticate('jwt', { session: false }), (req, res) => {
+app.get('/movies', (req, res) => {
   Movies.find().then((movies) => {
     res.status(201).json(movies);
   })
